49a8b8518860fc252ca7fd40377b266f9f4c94ba
n7commanderjohn <kimgears2@gmail.com>
2020-06-22 11:11:27 -0400

    solution 1 method organization

--- index.js
+++ index.js
@@ -28,49 +28,58 @@ function executeSolution1() {
     /**
      * @param {Array} taskArr
      */
-    async function doAsync(taskArr, num) {
-        // //A. Mehhh method without using async/await.
-        // const callback = (iterArg) => () => {
-        //     const task = iterArg.next().value;
-        //     let promise;
-        //     if (task) {
-        //         if (Array.isArray(task)) {
-        //             const promiseArr = task.map(subTask => asyncOp(subTask));
-        //             promise = Promise.all(promiseArr);
-        //         } else {
-        //             promise = asyncOp(task);
-        //         }
-        //         promise.then(callback(iterArg));
-        //     }
-        // };
-        // const iter = arr[Symbol.iterator]();
-        // asyncOp(iter.next().value, callback(iter));
-        // //B. Async/Await Method if asyncOp was async
-        // for (const task of taskArr) {
-        //     const isAnArrayOfTasks = Array.isArray(task);
-        //     if (isAnArrayOfTasks) {
-        //         const promiseArr = task.map(subTask => asyncOp(subTask));
-        //         await Promise.all(promiseArr);
-        //     }
-        //     else {
-        //         await asyncOp(task);
-        //     }
-        // }
-        //C. Workaround for if asyncOp isn't async
-        console.log(`starting async function ${num}`);
-        async function promisedAsyncOp(task) {
-            return new Promise(resolve => asyncOp(task, resolve));
-        }
+    async function doAsync(taskArr, num, methodNum = 3) {
 
-        for (let task of taskArr) {
-            if (Array.isArray(task)) {
-                await Promise.all(task.map(subtask => promisedAsyncOp(subtask)));
-            }
-            else {
-                await promisedAsyncOp(task);
+        //A. Mehhh method without using async/await.
+        if (methodNum == 1) {
+            const callback = (iterArg) => () => {
+                const task = iterArg.next().value;
+                let promise;
+                if (task) {
+                    if (Array.isArray(task)) {
+                        const promiseArr = task.map(subTask => asyncOp(subTask));
+                        promise = Promise.all(promiseArr);
+                    } else {
+                        promise = asyncOp(task);
+                    }
+                    promise.then(callback(iterArg));
+                }
+            };
+        }   
+
+        //B. Async/Await Method if asyncOp was async
+        else if (methodNum == 2) {
+            const iter = arr[Symbol.iterator]();
+            asyncOp(iter.next().value, callback(iter));
+            for (const task of taskArr) {
+                const isAnArrayOfTasks = Array.isArray(task);
+                if (isAnArrayOfTasks) {
+                    const promiseArr = task.map(subTask => asyncOp(subTask));
+                    await Promise.all(promiseArr);
+                }
+                else {
+                    await asyncOp(task);
+                }
             }
         }
-        console.log(`finished async function ${num}`);
+        
+        //C. Workaround for if asyncOp isn't async
+        else if (methodNum == 3) {
+            console.log(`starting async function ${num}`);
+            async function promisedAsyncOp(task) {
+                return new Promise(resolve => asyncOp(task, resolve));
+            }
+    
+            for (let task of taskArr) {
+                if (Array.isArray(task)) {
+                    await Promise.all(task.map(subtask => promisedAsyncOp(subtask)));
+                }
+                else {
+                    await promisedAsyncOp(task);
+                }
+            }
+            console.log(`finished async function ${num}`);
+        }
     }
 
     const input1 = [
